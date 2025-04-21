0d9f79793f4242fd9239fef5b036b8e162e6e3d7
master <https://github.com/NikitaNeupokoev/homework_bot>
2025-04-21 17:21:06 +0700

    'update files'

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -2,12 +2,8 @@ from rest_framework import permissions
 
 
 class IsAdmin(permissions.BasePermission):
-    """
-    Разрешает доступ только администраторам.
-    """
+    """Разрешает доступ только администраторам."""
 
     def has_permission(self, request, view):
-        """
-        Проверяет, является ли пользователь администратором.
-        """
+        """Проверяет, является ли пользователь администратором."""
         return request.user.is_authenticated and request.user.is_admin
