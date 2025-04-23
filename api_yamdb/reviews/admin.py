c273efa41edab8a4ae656c7db21b250d3143e690
Nikita Neupokoev <n.neupokoev154@yandex.ru>
2025-04-23 18:39:20 +0700

    update admin.py

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -11,6 +11,33 @@ from .models import (
 )
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    """Административная панель для модели Category."""
+
+    list_display = ('pk', 'name', 'slug')
+    search_fields = ('name', 'slug')
+    empty_value_display = '-пусто-'
+    list_editable = ('name', 'slug')
+
+
+class GenreAdmin(admin.ModelAdmin):
+    """Административная панель для модели Genry."""
+
+    list_display = ('pk', 'name', 'slug')
+    search_fields = ('name', 'slug')
+    empty_value_display = '-пусто-'
+    list_editable = ('name', 'slug')
+
+
+class TitlesAdmin(admin.ModelAdmin):
+    """Административная панель для модели Category."""
+
+    list_display = ('pk', 'name', 'year', 'category')
+    search_fields = ('name', 'category')
+    empty_value_display = '-пусто-'
+    list_editable = ('name', 'category')
+
+
 class ReviewAdmin(admin.ModelAdmin):
     """Административная панель для модели Review."""
 
@@ -20,9 +47,16 @@ class ReviewAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
+class CommentAdmin(admin.ModelAdmin):
+    """Административная панель для модели Comment."""
+    list_display = ('pk', 'review', 'author', 'text', 'pub_date')
+    search_fields = ('text',)
+    empty_value_display = '-пусто-'
+
+
 admin.site.register(CustomUser, UserAdmin)
 admin.site.register(Review, ReviewAdmin)
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Title)
-admin.site.register(Comment)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Title, TitlesAdmin)
+admin.site.register(Comment, CommentAdmin)
