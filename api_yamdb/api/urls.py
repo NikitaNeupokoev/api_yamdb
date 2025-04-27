620d76a1bfc35494ccf82f8fc9f298c20fe3de52
Arseniy <y4ne242@gmail.com>
2025-04-27 11:18:50 +0300

    изменение urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,47 +1,12 @@
 from django.urls import include, path
 
+from api.v1.users.views import signup, get_token
+
 app_name = 'api'
 
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-
-from api.v1.users.views import signup, get_token, UserViewSet
-from django.urls import include, path
-from rest_framework.routers import SimpleRouter
-
-from api.v1.reviews.views import (
-    CategoryViewSet,
-    GenreViewSet,
-    TitleViewSet,
-    ReviewViewSet,
-    CommentViewSet
-)
-
-
-app_name = 'users'
-
-router_v1 = SimpleRouter()
-router_v1.register('categories', CategoryViewSet, basename='category')
-router_v1.register('genres', GenreViewSet, basename='genre')
-router_v1.register('titles', TitleViewSet, basename='title')
-router_v1.register(
-    r'titles/(?P<title_id>[0-9]+)/reviews',
-    ReviewViewSet,
-    basename='review'
-)
-router_v1.register(
-    r'titles/(?P<title_id>[0-9]+)/reviews/(?P<review_id>[0-9]+)/comments',
-    CommentViewSet,
-    basename='comment'
-)
-router_v1.register(
-    'users',
-    UserViewSet,
-    basename='users'
-)
-
 urlpatterns = [
+    path('', include('api.v1.reviews.urls')),
+    path('users/', include('api.v1.users.urls')),
     path('auth/signup/', signup, name='signup'),
     path('auth/token/', get_token, name='get_token'),
-    path('', include(router_v1.urls)),
 ]
