2f990cdc7afaf60a5f234ca6060011008a9f2100
master <https://github.com/NikitaNeupokoev/homework_bot>
2025-04-28 17:26:11 +0700

    'update'

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -7,17 +7,15 @@ from api_yamdb.constants import MIN_YEAR
 def validate_year(values):
     """Валидации значения year."""
     current_year = timezone.now().year
+
     if MIN_YEAR > values:
-        error_message = (
-            f'год выпуска не может быть меньше {MIN_YEAR}.'
+        raise ValidationError(
+            f'Год выпуска не может быть меньше {MIN_YEAR}.'
         )
 
-    elif values > current_year:
-        error_message = (
-            f'год выпуска не может быть больше {current_year}.'
+    if values > current_year:
+        raise ValidationError(
+            f'Год выпуска не может быть больше {current_year}.'
         )
 
-    else:
-        return values
-
-    raise ValidationError(error_message)
+    return values
