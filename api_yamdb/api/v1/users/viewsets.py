fc80678357d4f6d354958050d0fd9b77e9cd0bc9
master <https://github.com/NikitaNeupokoev/homework_bot>
2025-04-29 10:51:27 +0700

    'update'

--- api_yamdb/api/v1/users/viewsets.py
+++ api_yamdb/api/v1/users/viewsets.py
@@ -1,6 +1,4 @@
-from rest_framework import filters
-from rest_framework import mixins
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets, filters
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
